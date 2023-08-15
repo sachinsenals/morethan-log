94e6a11d3c0452c69fdf9bd626ad4f3b382246d6
Sachin Senal <114604792+sachinsenals@users.noreply.github.com>
2023-08-15 16:22:56 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -46,21 +46,21 @@ const CONFIG = {
   //     siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
   //   },
   // },
-  utterances: {
-    enable: true,
-    config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      "issue-term": "og:title",
-      label: "💬 Utterances",
-    },
-  },
-  cusdis: {
-    enable: false,
-    config: {
-      host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
-    },
-  },
+  // utterances: {
+  //   enable: true,
+  //   config: {
+  //     repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+  //     "issue-term": "og:title",
+  //     label: "💬 Utterances",
+  //   },
+  // },
+  // cusdis: {
+  //   enable: false,
+  //   config: {
+  //     host: "https://cusdis.com",
+  //     appid: "", // Embed Code -> data-app-id value
+  //   },
+  // },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
