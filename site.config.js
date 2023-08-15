5d6c1b28a126617bd008f9d10f1471c4f1ef4fff
Sachin Senal <114604792+sachinsenals@users.noreply.github.com>
2023-08-15 16:27:22 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -33,34 +33,34 @@ const CONFIG = {
     pageId: process.env.NOTION_PAGE_ID,
   },
 
-  // // plugin configuration (optional)
-  // googleAnalytics: {
-  //   enable: false,
-  //   config: {
-  //     measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
-  //   },
-  // },
-  // googleSearchConsole: {
-  //   enable: false,
-  //   config: {
-  //     siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
-  //   },
-  // },
-  // utterances: {
-  //   enable: true,
-  //   config: {
-  //     repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-  //     "issue-term": "og:title",
-  //     label: "💬 Utterances",
-  //   },
-  // },
-  // cusdis: {
-  //   enable: false,
-  //   config: {
-  //     host: "https://cusdis.com",
-  //     appid: "", // Embed Code -> data-app-id value
-  //   },
-  // },
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: "sachinsenals/morethan-log",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
